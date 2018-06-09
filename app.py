487cdb15036fdefea04da2c3185d0986ff64a50d
Nnanna Ijioma <nnannaijioma@yahoo.com>
2018-06-09 11:22:36 +0200

    First Graph

--- app.py
+++ app.py
@@ -18,6 +18,23 @@ app.layout = html.Div(children=[
         Displays Mapss
     '''),
     
+    dcc.Graph(
+        id='basic-interactions',
+        figure={
+            'data': [
+                {
+                    'x': [1, 2, 3, 4],
+                    'y': [4, 1, 3, 5],
+                    'text': ['a', 'b', 'c', 'd'],
+                    'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
+                    'name': 'Trace 1',
+                    'mode': 'markers',
+                    'marker': {'size': 12}
+                }
+            ]
+        }
+    ),
+    
     html.H2(children='''
         QC plots
     '''),
